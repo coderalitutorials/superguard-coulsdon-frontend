05a25fefb4271fc4ce960ae2d8f2a2c619ce7b16
coderalitutorials <razamuneer670@gmail.com>
2026-08-14 00:48:54 -0700

    updation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,28 +11,24 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 
 
-import CertifiedLogosSection from '../components/CertifiedLogosSection'
+
 
 import AboutIntroSection from '../components/AboutIntro'
 import MissionBannerSection from '../components/MissionBannerSection'
 
-import PrimeShieldTestimonials from '../components/PrimeShieldTestimonials'
 import SuperGuardServicesSection from '../components/IronServices'
-import WhyChooseSuperGuard from '../components/WhyChoosePrimeShield'
-import SuperGuardStatsSection from '../components/StatsSection'
+
+
 
 const Home = () => {
   return (
     <div>
       <HeroSection/>
       <AboutIntroSection/>
-      {/* <SuperGuardStatsSection/> */}
-      {/* <CertifiedLogosSection/>  */}
+    
         <SuperGuardServicesSection/>
       <MissionBannerSection/>
-    
-    {/* <WhyChooseSuperGuard/> */}
-       {/* <PrimeShieldTestimonials/> */}
+   
    
      
     </div>
